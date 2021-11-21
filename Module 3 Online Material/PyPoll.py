9ac999440b2fef2983dd856697a728dafd089dd0
Tyson Herrin <taherrin92@gmail.com>
2021-11-20 19:08:39 -0600

    election audit code.

--- Module 3 Online Material/PyPoll.py
+++ Module 3 Online Material/PyPoll.py
@@ -12,22 +12,53 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join('analysis','election_analysis.txt')
 
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
      file_reader = csv.reader(election_data)
      
+     headers = next(file_reader)
+
      for row in file_reader:
-          print(row[0])
+          
+
 #The total number of votes cast
 
-
+          total_votes += 1
 #complete list of candidates with votes
-
+          candidate_name = row[2]
+          
+          if candidate_name not in candidate_options:
+               candidate_options.append(candidate_name)
+               
+               candidate_votes[candidate_name] = 0 
+          
+          candidate_votes[candidate_name] +=1
+     
 
 #percentage of votes for each candidate
 
-
-#total number of votes for each candidate
-
-
+     for candidate_name in candidate_votes:
+          votes = candidate_votes[candidate_name]
+          vote_percentage = float(votes) / float(total_votes) * 100
 #winner based on popular vote
+     
+
+          if (votes > winning_count) and (vote_percentage > winning_percentage):
+               winning_count = votes
+               winning_percentage = vote_percentage
+               winning_candidate = candidate_name
+     winning_candidate_summary = (
+                                   f'-------------------------\n'
+                                   f'Winner: {winning_candidate}\n'
+                                   f'Winning Vote Count: {winning_count:,}\n'
+                                   f'Winning Percentage: {winning_percentage:.1f}\n'
+                                   f'-------------------------\n'
+                                   )
+     print(winning_candidate_summary)
 election_data.close()
